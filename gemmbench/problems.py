962706ae41309db7fcacdd26a521ff6e6599ff3b
saienduri <77521230+saienduri@users.noreply.github.com>
2024-10-09 13:33:04 -0700

    Update problems.py

--- gemmbench/problems.py
+++ gemmbench/problems.py
@@ -901,6 +901,7 @@ def surya():
     yield GEMM("benoit_test", 1024, 1024, 1024, "N", "T", "fp16")
     yield GEMM("benoit_test", 2048, 2048, 2048, "N", "T", "fp16")
     yield GEMM("benoit_test", 4096, 4096, 4096, "N", "T", "fp16")
+    yield GEMM("benoit_test", 8192, 8192, 8192, "N", "T", "fp16")
     
 
 configurations = [Configuration(0, 0, 0)]
